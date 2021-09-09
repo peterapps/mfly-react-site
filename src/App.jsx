c425de6df4679a52596de8504abc6cdf0ea92827
Peter Linder <palinder@umich.edu>
2021-09-09 15:27:36 -0400

    M-Fly navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+import "../css/Navbar.css";
+import logo from "../img/mfly_white.png";
+
+const links = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Members", path: "/members" },
+  { name: "Fleet", path: "/fleet" },
+  { name: "Sponsors", path: "/sponsors" },
+  { name: "Contact", path: "/contact" },
+];
+
+export default function Navbar() {
+  const { pathname } = useLocation();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={"mfly-navbar" + (open ? " responsive" : "")}>
+      <div className="logo">
+        <img src={logo} alt="M-Fly" className="logo" />
+      </div>
+      {links.map(({ name, path }) => (
+        <Link to={path} className={path === pathname ? "active" : ""}>
+          {name}
+        </Link>
+      ))}
+      <a href="#" className="icon" onClick={() => setOpen(!open)}>
+        <FontAwesomeIcon icon={faBars} size="lg" color="white" />
+      </a>
+    </div>
+  );
+}
