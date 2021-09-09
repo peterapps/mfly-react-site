13236c1bd9822bc7cfe260d341d8abaf887ec8d2
Peter Linder <palinder@umich.edu>
2021-09-09 16:56:19 -0400

    Use HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import "aos/dist/aos.css";
